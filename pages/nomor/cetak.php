2368ee038e17c010cb2c084635cab42ac65d2a86
aderahman9908 <aderahman9908@gmail.com>
2024-03-03 12:18:55 +0700

    Remove function format hari

--- pages/nomor/cetak.php
+++ pages/nomor/cetak.php
@@ -34,25 +34,3 @@ function cetak($no_antrian, $code_antrian, $config)
     }
 }
 
-
-function hariIndo($hariInggris)
-{
-    switch ($hariInggris) {
-        case 'Sunday':
-            return 'Minggu';
-        case 'Monday':
-            return 'Senin';
-        case 'Tuesday':
-            return 'Selasa';
-        case 'Wednesday':
-            return 'Rabu';
-        case 'Thursday':
-            return 'Kamis';
-        case 'Friday':
-            return 'Jumat';
-        case 'Saturday':
-            return 'Sabtu';
-        default:
-            return 'hari tidak valid';
-    }
-}
